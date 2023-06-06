eefd81af6715bc73dae6f145a88b49dba735d1c2
Solunsky <kevinhox69@gmail.com>
2023-06-06 09:26:13 +0300

    Update mockDataProvider.ts

--- src/api/mockDataProvider.ts
+++ src/api/mockDataProvider.ts
@@ -143,7 +143,7 @@ export class MockDataProvider implements DataProvider {
           id: 0,
           name: "Capcom Showcase",
           date: new Date("2023-06-12T22:00:00+00:00"),
-          description: `Capcom провёдет получасовую трансляцию, на которой возможно покажут Exoprimal, Pragmata, и с ещё меньшим шансом - анонс RE9`,
+          description: `Capcom в рамках SGF провёдет получасовую трансляцию, на которой возможно покажут Exoprimal, Pragmata, и с ещё меньшим шансом - анонс RE9`,
           rumors: [],
           youtubeLink: "https://www.youtube.com/@CapcomUSA",
         },
