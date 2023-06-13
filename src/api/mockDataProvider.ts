b7102d4cff628390267a1f76003d3c8b25b11458
Solunsky <kevinhox69@gmail.com>
2023-06-13 07:13:02 +0300

    Update mockDataProvider.ts

--- src/api/mockDataProvider.ts
+++ src/api/mockDataProvider.ts
@@ -15,15 +15,6 @@ export class MockDataProvider implements DataProvider {
         },
         {
           id: 0,
-          name: "Ubisoft Forward",
-          date: new Date("2023-06-12T17:00:00+00:00"),
-          description: `Выставка от Ubisoft возвращается, чтобы поделиться новостями о уже анонсированных проектах и дать новые анонсы. Beyond Good and Evil 2 NEVER.`,
-          rumors: [],
-          twitchLink: "https://www.twitch.tv/ubisoft",
-          youtubeLink: "https://www.youtube.com/c/ubisoft",
-        },
-        {
-          id: 0,
           name: "THQ Nordic Digital Showcase",
           date: new Date("2023-08-11T19:00:00+00:00"),
           description: `Будет представлена новая информация о Alone in the Dark, Outcast 2, Trine 5 и других неуказанных играх, а также несколько новых анонсов.`,
@@ -57,14 +48,6 @@ export class MockDataProvider implements DataProvider {
           description: `Команда поделится информацией о своих грядущих играх или анонсирует проекты, о которых прежде не рассказывала. Стоит ожидать больше информации о Like a Dragon 8.`,
           rumors: [],
           youtubeLink: "https://www.youtube.com/watch?v=UG4HE9Q3wek",
-        },
-        {
-          id: 0,
-          name: "Capcom Showcase",
-          date: new Date("2023-06-12T22:00:00+00:00"),
-          description: `Capcom в рамках SGF провёдет получасовую трансляцию, на которой как минимум точно покажут Dragon’s Dogma 2, Exoprimal и Ghost Trick.`,
-          rumors: [],
-          youtubeLink: "https://www.youtube.com/@CapcomUSA",
         },
         {
           id: 0,
